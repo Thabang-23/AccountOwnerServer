adafe474396fe3a3355114a48ab7ac5fa16e2ab5
thabang-23 <thabang.khoza@sovtech.com>
2022-12-11 15:54:04 +0200

    initialCommit

---
+++ AccountOwnerServer/Program.cs
@@ -0,0 +1,46 @@
+using AccountOwnerServer.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.confg"));
+
+// Add services to the container.
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+builder.Services.ConfigureLoggerService();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
